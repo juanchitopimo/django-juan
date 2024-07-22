8c4f6edcdca00a7ddf8571795c47824b334c55cb
Juan Pinson Moreno <juan@fourrooms.co.uk>
2024-07-22 15:03:30 +0000

    creating new app

--- about/views.py
+++ about/views.py
@@ -1,7 +1,6 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 # Create your views here.
-def index(request):
-    return HttpResponse('Hello_world')
-    
+
+def about_me(request):
+    return HttpResponse("This would be the about page")
